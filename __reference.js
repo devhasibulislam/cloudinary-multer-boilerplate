3f8753a9960ccc0fde5b29600a9c109d46b7dfe4
hasibulislam999 <hasib143sl@gmail.com>
2022-10-30 21:16:57 +0600

    reference added to resolved

---
+++ __reference.js
@@ -0,0 +1,11 @@
+/**
+ * Uploading Images to Cloudinary Using Multer and ExpressJS
+ * https://medium.com/@joeeasy_/uploading-images-to-cloudinary-using-multer-and-expressjs-f0b9a4e14c54
+ * ---------------------------------------------------------------------------------------------------
+ * Upload and delete images with a React App using Cloudinary API
+ * https://priyal-babel.medium.com/upload-and-delete-images-with-a-react-app-using-cloudinary-api-32565d11d760
+ * -----------------------------------------------------------------------------------------------------------
+ * Multer Storage Cloudinary
+ * https://www.npmjs.com/package/multer-storage-cloudinary
+ * -------------------------------------------------------
+ */
